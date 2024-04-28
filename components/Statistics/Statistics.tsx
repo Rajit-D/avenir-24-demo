c04351418d591b1c70888c3df7323e32f4085f6c
Rajit-D <rajitdutta2002@gmail.com>
2024-04-28 16:06:50 +0530

    stats changed

--- components/Statistics/Statistics.tsx
+++ components/Statistics/Statistics.tsx
@@ -2,20 +2,23 @@ import React from "react";
 
 const Statistics = () => {
   return (
-    <div className="stats stats-vertical lg:stats-horizontal shadow">
-      <div className="stat">
+    <div className="w-full flex justify-around items-center bg-opacity-40 z-2 backdrop-brightness-200 backdrop-blur-[1.7px] bg-black">
+      <div className="pt-4 pb-4">
         <div className="stat-title">Downloads</div>
         <div className="stat-value">31K</div>
         <div className="stat-desc">Jan 1st - Feb 1st</div>
       </div>
-
-      <div className="stat">
+      <div className="">
         <div className="stat-title">New Users</div>
         <div className="stat-value">4,200</div>
         <div className="stat-desc">↗︎ 400 (22%)</div>
       </div>
-
-      <div className="stat">
+      <div className="">
+        <div className="stat-title">New Registers</div>
+        <div className="stat-value">1,200</div>
+        <div className="stat-desc">↘︎ 90 (14%)</div>
+      </div>
+      <div className="">
         <div className="stat-title">New Registers</div>
         <div className="stat-value">1,200</div>
         <div className="stat-desc">↘︎ 90 (14%)</div>
