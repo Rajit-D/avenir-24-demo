4f34995c75d8241412af79488ad0859fd4793c00
unknown <abhinilnath10@gmail.com>
2024-05-03 15:46:43 +0530

    Added dynamic routing for wing landing page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,20 +3,21 @@
 import React, { useEffect, useRef } from "react";
 import EventsCard from "@/components/EventsCard/EventsCard";
 import WingCard from "@/components/WingCard/WingCard";
+import { events } from "@/lib/constants";
+import Link from "next/link";
 
 const Events = () => {
   return (
     <div className="text-black w-screen md:h-auto h-auto flex justify-center items-center bg-opacity-20 z-2 backdrop-brightness-100 backdrop-blur-[1.7px]">
       <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 mt-[150px] mb-[100px]">
         {/* <EventsCard /> */}
-        <WingCard wing="Eloquense"/>
-        <WingCard wing="Cybernix"/>
-        <WingCard wing="Nirmaan"/>
-        <WingCard wing="Robonix"/>
-        <WingCard wing="Illustro"/>
-        <WingCard wing="Flagship events"/>
-        <WingCard wing="eSports"/>
-        <WingCard wing="Fun"/>
+        
+
+        {events.map((eventName, index) => (
+            <Link href={`/${eventName}`} key={index}>
+             <WingCard wing={eventName}/>
+            </Link>
+          ))}
       </div>
     </div>
   );
--- components/EventsCard/EventsCard.tsx
+++ components/EventsCard/EventsCard.tsx
@@ -2,14 +2,23 @@ import React from "react";
 import EventsButton from "../EventsButton/EventsButton";
 import { BackgroundGradient } from "../ui/background-gradient";
 
-const EventsCard = () => {
+const EventsCard = ({name,description} : {name:string,description:string}) => {
+  function shortenDescription(description: string, maxLength: number) {
+    // Check if the description length is greater than maxLength
+    if (description.length > maxLength) {
+      // Truncate the description to maxLength characters and append "..."
+      return description.slice(0, maxLength) + "...";
+    } else {
+      // If the description is already shorter than maxLength, return it as is
+      return description;
+    }
+  }
   return (
     <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-1 bg-[#1B1B1E]">
       <div className="flex flex-col p-6 mx-auto md:max-w-[300px] max-w-[220px] text-center text-white rounded-[22px] shadow xl:p-8">
-        <h3 className="mb-4 text-2xl font-strike">Event name</h3>
+        <h3 className="mb-4 text-2xl font-strike">{name}</h3>
         <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
-          exercitationem?
+         {description.slice(0,100)}
         </p>
         <EventsButton />
       </div>
