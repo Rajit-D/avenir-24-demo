86ad193bc63cb6f6721b9eb58588ac243a113c32
Rajit-D <rajitdutta2002@gmail.com>
2024-05-07 23:54:59 +0530

    stats page fixed

--- components/Statistics/Statistics.tsx
+++ components/Statistics/Statistics.tsx
@@ -6,14 +6,14 @@ const Statistics = () => {
   return (
     <div className="w-full mt-5 flex md:flex-row flex-col md:justify-around justify-center items-center bg-opacity-40 z-2 backdrop-brightness-200 backdrop-blur-[10px] bg-black md:p-0 p-3 font-mont font-bold">
       <div className="w-full flex justify-around items-center">
-        <div className="pt-4 pb-4">
+        <div className="pt-4 pb-4 relative md:right-0 right-4">
           <div className="stat-title flex">
             Footfall <IoFootstepsOutline className="relative top-1 ml-1" />
           </div>
           <div className="stat-value md:text-[35px] text-[25px] font-bold">5,000+</div>
           <div className="stat-desc">1st, 2nd and 3rd day</div>
         </div>
-        <div className="pt-4 pb-4">
+        <div className="pt-4 pb-4 relative md:right-0 right-14">
           <div className="stat-title">Sponsors</div>
           <div className="stat-value md:text-[35px] text-[25px] font-bold">25+</div>
           <div className="stat-desc">↗︎ 7 (38%)</div>
