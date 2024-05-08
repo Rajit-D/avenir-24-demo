d436221b47b0523d04b962077ee7c593b7414388
Rajit-D <rajitdutta2002@gmail.com>
2024-05-08 21:36:11 +0530

    events limit changed

--- utils/multi.ts
+++ utils/multi.ts
@@ -3,26 +3,26 @@ export const multipleEvents = {
     {
       name: "Robo Soccer",
       min: 1,
-      max: 4,
+      max: 3,
       price: 350,
     },
     {
       name: "Terra Rover",
       min: 1,
-      max: 4,
+      max: 3,
       price: 400,
     },
 
     {
       name: "Robo War",
       min: 1,
-      max: 4,
+      max: 3,
       price: 500,
     },
     {
       name: "COMBO 1 (Robo Soccer, Robo War, Terra Rover)",
       min: 1,
-      max: 4,
+      max: 3,
       price: 1150,
     },
   ],
@@ -43,36 +43,36 @@ export const multipleEvents = {
   eloquense: [
     {
       name: "Dimensional Discourse",
-      min: 2,
-      max: 2,
+      min: 1,
+      max: 1,
       price: 200,
     },
     {
       name: "Cosmic Conundrum ",
-      min: 2,
-      max: 2,
+      min: 1,
+      max: 1,
       price: 150,
     },
   ],
   nirman: [
     {
       name: "Bridge-O-Mania",
-      min: 2,
-      max: 3,
+      min: 1,
+      max: 2,
       price: 120,
     },
   ],
   fun: [
     {
       name: "Treasure Hunt",
-      min: 4,
-      max: 4,
+      min: 3,
+      max: 3,
       price: 100,
     },
     {
       name: "Beg! Borrow! Steal!",
       min: 1,
-      max: 4,
+      max: 3,
       price: 100,
     },
   ],
