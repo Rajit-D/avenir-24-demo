cded9fdadaaa5ebf84c22e2540b2a8e159731265
adarshgupta404 <gadarsh747@gmail.com>
2024-05-10 00:13:40 +0530

    commented teams

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Teams = () => {
   return (
     <div className="w-dvw lg:flex justify-center items-center bg-opacity-20 lg:pt-0 z-2 h-dvh overflow-y-auto backdrop-brightness-100 backdrop-blur-[1.7px]">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 py-10 px-5 md:px-12 lg:px-36 grid-cols-1 md:gap-20 lg:gap-12 gap-4 md:mt-20 lg:mt-[200px] w-full">
+      {/* <div className="grid md:grid-cols-2 lg:grid-cols-3 py-10 px-5 md:px-12 lg:px-36 grid-cols-1 md:gap-20 lg:gap-12 gap-4 md:mt-20 lg:mt-[200px] w-full">
       <TeamsCard/>
       <TeamsCard/>
       <TeamsCard/>
@@ -13,8 +13,8 @@ const Teams = () => {
       <TeamsCard/>
       <TeamsCard/>
       <TeamsCard/>
-      </div>
-      {/* <h1 className="font-strike text-red-400 md:text-[80px] text-[50px] text-center text-wrap md:p-0 p-[10px]">This page is under progress</h1> */}
+      </div> */}
+      <h1 className="font-strike text-red-400 md:text-[80px] text-[50px] text-center text-wrap md:p-0 p-[10px]">This page is under progress</h1>
     </div>
   );
 };
