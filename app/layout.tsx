7ff69acf8fa1719f96f08defe49b420a9fa47cbb
adarshgupta404 <gadarsh747@gmail.com>
2024-05-10 00:33:12 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,9 @@ export const metadata: Metadata = {
   keywords: ["Avenir'24", "avenirnsec", "avenir nsec", "avenirnsec24", "Avenir 24", "avenir 24", "avenir nsec 24", "Avenir", "avenir"],
   title: {
     default:"AVENIR'24",
-    template: `%s | AVENIR'24`
+    template: `%s | AVENIR'24`,
   },
+  icons:'https://www.avenirnsec.live/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.a2374b39.png&w=1920&q=75',
   openGraph:{
     description:"The official tech fest of PHOENIX",
     images:['https://www.avenirnsec.live/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.a2374b39.png&w=1920&q=75']
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><link rel="icon" href="/assets/phnxlogo.png" type="image/png" sizes="any" /></head>
       <body className={inter.className}>
         <div className="w-full bg-black bg-grid-white/[0.4] relative flex items-center justify-center flex-col">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_40%,black)] "></div>
